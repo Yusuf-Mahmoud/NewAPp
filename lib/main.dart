5dd4b178287971b0c06065d1f871a0446955b183
Yusuf-Mahmoud <amegosss789@gmail.com>
2024-03-09 18:00:59 +0200

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:todo/Theme.dart';
 import 'package:todo/homescreen.dart';
 import 'package:todo/tabs/settings/setting_provider.dart';
+import 'package:todo/theme.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
@@ -16,8 +16,8 @@ class TodoApp extends StatelessWidget {
     SettingProvider settingProvider = Provider.of<SettingProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
+      theme: apptheme.lighttheme,
+      darkTheme: apptheme.darktheme,
       themeMode: settingProvider.themeMode,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
